88a2b57ac73ea33f248f480bba43dc0efdfc8344
v-almeida <almeida.vitor20000@gmail.com>
2026-01-29 18:01:21 -0300

    changing some stuffs

--- Case01/src/Program.java
+++ Case01/src/Program.java
@@ -1,21 +1,27 @@
 public class Program {
     public static void main(String[] args) throws Exception {
         
-        // Livro 01
+        /*Livro 01:
+        Código: 1598FHK
+        Título: Core Java 2
+        Autores: Cay S. Horstmann e Gary Cornell
+        ISBN: 0130819336
+        Ano: 2005 */
 
         Livro livro1 = new Livro();
+        System.out.println("\nLivro 01");
 
-        System.out.println("Livro 01");
-        
         livro1.codigo = "1598FHK";
         livro1.titulo = "Core Java 2";
+        livro1.autores = new String[] {
+            "Cay S. Horstmann",
+            "Gary Cornell"
+        };
         livro1.isbn = "0130819336";
         livro1.ano = 2005;
 
-        System.out.format("Código: %s \nTitulo: %s \nISBN: %s \nAno: %d ", livro1.codigo,
-            livro1.titulo, livro1.isbn, livro1.ano
+        System.out.format("\nCodigo: %s \nTitulo: %s \nAutores: %s \nISBN: %s \nAno: %d\n ", 
+            livro1.codigo, livro1.titulo, String.join(", ", livro1.autores), livro1.isbn, livro1.ano
         );
-
-
     }
 }
