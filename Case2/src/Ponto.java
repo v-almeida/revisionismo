27edfaf50af96e577a9a0b6feb05f2491077c28e
v-almeida <almeida.vitor20000@gmail.com>
2026-02-04 19:08:38 -0300

    Working on case 2...

---
+++ Case2/src/Ponto.java
@@ -0,0 +1,45 @@
+/*Cenário 2: Crie uma classe que represente um Ponto no espaço bidimensional. Na classe
+Programa efetue as operações listadas abaixo. Para realização dos cálculos de elevação ao
+quadrado utilizar Math.pow(valor, 2) e para extração da raiz quadrada utilizar Math.sqrt(valor).
+(Não precisa usar Model-View-Controller MVC)  
+
+1. Crie um objeto ponto1 usando o primeiro construtor;
+2. Crie um objeto ponto2 na posição (2,5);
+3. Calcule a distância do ponto1 ao ponto2;
+4. Calcule a distância do ponto2 às coordenadas (7,2);
+5. Altere o valor de x para 10 no ponto1;
+6. Altere o valor de y para 3 no ponto1; 
+
+ O construtor Ponto( ): cria um ponto na origem (0,0);
+ O Ponto(x: double, y: double): nas coordenadas x e y; 
+
+*/
+
+
+public class Ponto {
+    
+    // classe que representa um Ponto no espaço bidimensional. 
+
+    double x;
+    double y;
+
+    // 1. Crie um objeto ponto1 usando o primeiro construtor;
+
+    public Ponto(){
+
+        x = 0;
+        y = 0;
+    }
+
+    // 2. Crie um objeto ponto2 na posição (2,5);
+
+    public Ponto(double x, double y){
+
+        this.x = x;
+        this.y = y;
+
+    }
+
+    // 3. Calcule a distância do ponto1 ao ponto2;
+
+}
