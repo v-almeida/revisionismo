4dcdfad525dc8b545a1256c15af260d8584d717e
v-almeida <almeida.vitor20000@gmail.com>
2026-02-09 18:54:00 -0300

    first book is done!

--- Case01/src/Program.java
+++ Case01/src/Program.java
@@ -9,7 +9,7 @@ public class Program {
         Ano: 2005 */
 
         Livro livro1 = new Livro();
-        System.out.println("\nLivro 01\n");
+        System.out.println("\nLivro 01");
 
         livro1.codigo = "1598FHK";
         livro1.titulo = "Core Java 2";
@@ -19,6 +19,11 @@ public class Program {
         };
         livro1.isbn = "0130819336";
         livro1.ano = 2005;
+
+        System.out.format("\nCodigo: %s \nTitulo: %s \nAutores: %s \nISBN: %s \nAno: %s \n\n",
+            livro1.codigo, livro1.titulo, String.join(",", livro1.autores),
+            livro1.isbn, livro1.ano
+        );
 
 
     }
