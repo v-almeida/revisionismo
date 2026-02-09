992b6794d7694badfeb84409c511c691787ae57e
v-almeida <almeida.vitor20000@gmail.com>
2026-02-09 18:49:02 -0300

    doin again case 1, practiting only

--- Case01/src/Program.java
+++ Case01/src/Program.java
@@ -9,22 +9,16 @@ public class Program {
         Ano: 2005 */
 
         Livro livro1 = new Livro();
-        System.out.println("\nLivro 1\n");
+        System.out.println("\nLivro 01\n");
 
         livro1.codigo = "1598FHK";
         livro1.titulo = "Core Java 2";
         livro1.autores = new String[]{
             "Cay S. Horstmann",
-            "Gary Cornell"
+             "Gary Cornell"
         };
         livro1.isbn = "0130819336";
         livro1.ano = 2005;
-
-        System.out.format("\nCodigo: %s \nTitulo: %s \nAutores: %s \nISBN: %s \nAno: %d",
-            livro1.codigo, livro1.titulo, String.join(",", livro1.autores), livro1.isbn,
-            livro1.ano
-        );
-
 
 
     }
