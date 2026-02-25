fc67baf995430fc247683e7a35da3ce02a5b9535
v-almeida <almeida.vitor20000@gmail.com>
2026-02-25 19:32:45 -0300

    problem 4 solved, point2 distance at coords 7,2

--- Case2/src/Program.java
+++ Case2/src/Program.java
@@ -14,10 +14,18 @@ public class Program {
         double distancia1 = ponto1.calcularDistancia(ponto2);
         System.out.printf("Distância entre o ponto1 ao ponto2: %.2f%n", distancia1);
 
+        // 4. Calcule a distância do ponto2 às coordenadas (7,2);
+
+        Ponto ponto3 = new Ponto(7,2);
+        double distancia2 = ponto2.calcularDistancia(ponto3);
+        System.out.printf("O calculo da distância do ponto2 pelas coodernadas (7,2) é: %.2f%n ",
+            distancia2
+        );
 
         // 5. Altere o valor de x para 10 no ponto1;
 
         ponto1.setX(10);
+        System.out.println("Valor de X alterado para " + ponto1.getX());
 
         // 6. Altere o valor de y para 3 no ponto1; 
 
