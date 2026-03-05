2fe6796742ac37485d1265592b93f85e35267d60
v-almeida <almeida.vitor20000@gmail.com>
2026-03-05 19:05:07 -0300

    added some setters to x and y

--- Case2/src/Ponto.java
+++ Case2/src/Ponto.java
@@ -56,6 +56,16 @@ public class Ponto {
         return y;
     }
 
+    // Setters
+
+    public void setX(double x){
+        this.x = x;
+    }
+
+    public void setY(double y){
+        this.y = y;
+    }
+
 
 
 
