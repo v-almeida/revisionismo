0a6539ef7a80f160eb22190d08f0987fa16f744f
v-almeida <almeida.vitor20000@gmail.com>
2026-03-10 19:19:30 -0300

    instacing livro1

--- Case01/src/Program.java
+++ Case01/src/Program.java
@@ -1,6 +1,7 @@
 public class Program {
     public static void main(String[] args) throws Exception {
 
-        
+        Livro livro1 = new Livro();
+        System.out.println("\nLivro 01\n");
     }
 }
