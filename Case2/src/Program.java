c1918600003c87b3fea2c61d56ba531ae35c5bdc
v-almeida <almeida.vitor20000@gmail.com>
2026-03-11 19:48:45 -0300

    doin case 2 again

--- Case2/src/Program.java
+++ Case2/src/Program.java
@@ -1,33 +1,6 @@
 public class Program {
     public static void main(String[] args) throws Exception {
         
-       // 1. Crie um objeto ponto1 usando o primeiro construtor; 
-       
-       Ponto ponto1 = new Ponto();
-
-       // 2. Crie um objeto ponto2 na posição (2,5); 
-
-       Ponto ponto2 = new Ponto(2,5);
-
-       // 4. Calcule a distância do ponto2 às coordenadas (7,2); 
-
-       double distancia1 = ponto1.calcularDistancia(ponto2);
-       System.out.printf("A distância do ponto2 às coodernadas (7,2) é %.2f ", distancia1);
-
-       // 5. Altere o valor de x para 10 no ponto1; 
-
-       ponto1.setX(10);
-
-       System.out.println("\nValor de x agora é " + ponto1.getX());
-
-       // 6. Altere o valor de y para 3 no ponto1; 
-
-       ponto1.setY(3);
-
-       System.out.println("Valor de y alterado para " + ponto1.getY());
-
-        
-
 
     }
 }
