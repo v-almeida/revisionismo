eb1b783e2af802e3bdfc140a60a804c96ea83db1
v-almeida <almeida.vitor20000@gmail.com>
2026-03-11 20:40:30 -0300

    doing the first question, creating an object ponto1 using the first constructor

--- Case2/src/Program.java
+++ Case2/src/Program.java
@@ -1,6 +1,9 @@
 public class Program {
     public static void main(String[] args) throws Exception {
         
+        //1. Crie um objeto ponto1 usando o primeiro construtor;
+
+        Ponto ponto1 = new Ponto();
 
     }
 }
